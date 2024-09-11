ec1c484346f296eba1d7693e20b18ea4cd6eb5e9
aeswdeveloper <aeswdeveloper@gmail.com>
2024-09-11 15:41:27 +0300

    Add files via upload
    6-Task2-veritabanıilklendirme: İller için ilklendirme yapıldı

---
+++ src/main/java/salih_korkmaz/dnm_1005/util/Constants.java
@@ -0,0 +1,8 @@
+package salih_korkmaz.dnm_1005.util;
+
+/**
+ * Prepared by ahmeterdem on 11.09.2024 for Project Tarim-Proje.
+ */
+public class Constants {
+    public static final int VERITABANI_INILIATIZATION_EXCEPTION_EXIT_CODE = 999;
+}
